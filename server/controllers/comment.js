fc8dfbadef1ca0a913343a02757901e8beb4f83d
Steven V <sgvillacis@gmail.com>
2024-03-20 14:14:15 -0400

    completed comment controller and modified routes

---
+++ server/controllers/comment.js
@@ -0,0 +1,91 @@
+import Comment from "../models/comment"
+import jwt from "jsonwebtoken";
+
+export const getComments = async (req, res) => {
+    try {
+        const { recipeId } = req.params
+        const comments = await Comment.find({recipeId: recipeId})
+        res.json(comments)
+    
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const createComment = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token) {
+            return res
+            .status(401)
+            .json({ error: "Authorization token not provided" });
+        }
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        const userId = decoded.id;
+
+        const { recipeId } = req.params
+        const newComment = new Comment({ ...req.body, recipeId, userId })
+        await newComment.save()
+        res.status(201).json(newComment)
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+
+}
+
+export const editComment = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token) {
+            return res
+            .status(401)
+            .json({ error: "Authorization token not provided" });
+        }
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        const userId = decoded.id;
+
+        const { id } = req.params
+        const comment = await Comment.findById(id) //try with userId
+
+        
+        //  potentially need to compare userid with comment id | need to make sure only comments made by the user can be edited.
+        if (comment.userId === userId) {
+
+            const userComment = await Comment.findByIdAndUpdate(id, req.body, { new: true, })
+            res.status(201).json(userComment)
+        }
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });
+    }
+}
+
+export const deleteComment = async (req, res) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        if(!token) {
+            return res
+            .status(401)
+            .json({ error: "Authorization token not provided" });
+        }
+        const decoded = jwt.verify(token, process.env.TOKEN_KEY);
+        const userId = decoded.id;
+        
+        const { id } = req.params
+        const comment = await Comment.findById(id)
+
+        if (comment.userId === userId){
+            const deleted = await Comment.findByIdAndDelete(id)
+            if (deleted) {
+                return res.status(200).send("Comment deleted");
+              }
+              throw new Error("Comment not found");
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: error.message });   
+    }
+}
--- server/routes/comment.js
+++ server/routes/comment.js
@@ -3,9 +3,9 @@ import * as controllers from "../controllers/comment";
 
 const router = Router();
 
-router.get("/", controllers.getComments); 
-router.get("/:id", controllers.getComment); 
-router.put("/edit/:id", controllers.editComment); 
+router.get("/:recipeId", controllers.getComments); 
+router.put("/edit/:id", controllers.editComment);
+router.post("/create/recipeId", controllers.createComment);
 router.delete("/delete/:id", controllers.deleteComment); 
 
 export default router;
